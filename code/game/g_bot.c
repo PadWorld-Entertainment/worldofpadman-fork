id-Software/Quake-III-Arena

fix a few potential buffer overwrite in Game VM

code/game/g_bot.c
@@ -817,19 +817,19 @@ void Svcmd_BotList_f( void ) {
 
 	trap_Print("^1name             model            aifile              funname\n");
 	for (i = 0; i < g_numBots; i++) {
-		strcpy(name, Info_ValueForKey( g_botInfos[i], "name" ));
+		Q_strncpyz(name, Info_ValueForKey( g_botInfos[i], "name" ), sizeof( name ));
 		if ( !*name ) {
 			strcpy(name, "UnnamedPlayer");
 		}
-		strcpy(funname, Info_ValueForKey( g_botInfos[i], "funname" ));
+		Q_strncpyz(funname, Info_ValueForKey( g_botInfos[i], "funname" ), sizeof( funname ));
 		if ( !*funname ) {
 			strcpy(funname, "");
 		}
-		strcpy(model, Info_ValueForKey( g_botInfos[i], "model" ));
+		Q_strncpyz(model, Info_ValueForKey( g_botInfos[i], "model" ), sizeof( model ));
 		if ( !*model ) {
 			strcpy(model, "visor/default");
 		}
-		strcpy(aifile, Info_ValueForKey( g_botInfos[i], "aifile"));
+		Q_strncpyz(aifile, Info_ValueForKey( g_botInfos[i], "aifile"), sizeof( aifile ));
 		if (!*aifile ) {
 			strcpy(aifile, "bots/default_c.c");
 		}

code/game/g_client.c
@@ -838,11 +838,11 @@ void ClientUserinfoChanged( int clientNum ) {
 	teamLeader = client->sess.teamLeader;
 
 	// colors
-	strcpy(c1, Info_ValueForKey( userinfo, "color1" ));
-	strcpy(c2, Info_ValueForKey( userinfo, "color2" ));
+	Q_strncpyz(c1, Info_ValueForKey( userinfo, "color1" ), sizeof( c1 ));
+	Q_strncpyz(c2, Info_ValueForKey( userinfo, "color2" ), sizeof( c2 ));
 
-	strcpy(redTeam, Info_ValueForKey( userinfo, "g_redteam" ));
-	strcpy(blueTeam, Info_ValueForKey( userinfo, "g_blueteam" ));
+	Q_strncpyz(redTeam, Info_ValueForKey( userinfo, "g_redteam" ), sizeof( redTeam ));
+	Q_strncpyz(blueTeam, Info_ValueForKey( userinfo, "g_blueteam" ), sizeof( blueTeam ));
 	
 	// send over a subset of the userinfo keys so other clients can
 	// print scoreboards, display models, and play custom sounds

PadWorld-Entertainment/worldofpadman-fork

code/game/g_bot.c
@@ -696,19 +696,19 @@ void Svcmd_BotList_f(void) {
 
 	trap_Print("^1name             model            aifile              funname\n");
 	for (i = 0; i < g_numBots; i++) {
-		strcpy(name, Info_ValueForKey(g_botInfos[i], "name"));
+		Q_strncpyz(name, Info_ValueForKey(g_botInfos[i], "name"), sizeof(name));
 		if (!*name) {
 			strcpy(name, "UnnamedPlayer");
 		}
-		strcpy(funname, Info_ValueForKey(g_botInfos[i], "funname"));
+		Q_strncpyz(funname, Info_ValueForKey(g_botInfos[i], "funname"), sizeof(funname));
 		if (!*funname) {
 			strcpy(funname, "");
 		}
-		strcpy(model, Info_ValueForKey(g_botInfos[i], "model"));
+		Q_strncpyz(model, Info_ValueForKey(g_botInfos[i], "model"), sizeof(model));
 		if (!*model) {
 			strcpy(model, "visor/default");
 		}
-		strcpy(aifile, Info_ValueForKey(g_botInfos[i], "aifile"));
+		Q_strncpyz(aifile, Info_ValueForKey(g_botInfos[i], "aifile"), sizeof(aifile));
 		if (!*aifile) {
 			strcpy(aifile, "bots/default_c.c");
 		}

code/game/g_client.c
@@ -817,7 +817,7 @@ void ClientUserinfoChanged(int clientNum) {
 	}
 
 	if (client->pers.connected == CON_CONNECTED) {
-		if (strcmp(oldname, client->pers.netname)) {
+		if (strcmp(oldname, client->pers.netname) != 0) {
 			trap_SendServerCommand(-1,
 								   va("print \"%s" S_COLOR_WHITE " renamed to %s\n\"", oldname, client->pers.netname));
 		}
@@ -853,8 +853,8 @@ void ClientUserinfoChanged(int clientNum) {
 	teamLeader = client->sess.teamLeader;
 
 	// colors
-	strcpy(c1, Info_ValueForKey(userinfo, "color1"));
-	strcpy(c2, Info_ValueForKey(userinfo, "syc_color"));
+	Q_strncpyz(c1, Info_ValueForKey(userinfo, "color1"), sizeof(c1));
+	Q_strncpyz(c2, Info_ValueForKey(userinfo, "syc_color"), sizeof(c2));
 
 	// send over a subset of the userinfo keys so other clients can
 	// print scoreboards, display models, and play custom sounds