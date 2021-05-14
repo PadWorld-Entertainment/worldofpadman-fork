id-Software/Quake-III-Arena

Fix negative frag/capturelimit causing an infinite map end loop
Reported by @vloup.

code/game/g_main.c
@@ -1380,6 +1380,12 @@ void CheckExitRules( void ) {
 		}
 	}
 
+	if ( g_fraglimit.integer < 0 ) {
+		G_Printf( "fraglimit %i is out of range, defaulting to 0\n", g_fraglimit.integer );
+		trap_Cvar_Set( "fraglimit", "0" );
+		trap_Cvar_Update( &g_fraglimit );
+	}
+
 	if ( g_gametype.integer < GT_CTF && g_fraglimit.integer ) {
 		if ( level.teamScores[TEAM_RED] >= g_fraglimit.integer ) {
 			trap_SendServerCommand( -1, "print \"Red hit the fraglimit.\n\"" );
@@ -1409,6 +1415,12 @@ void CheckExitRules( void ) {
 				return;
 			}
 		}
+	}
+
+	if ( g_capturelimit.integer < 0 ) {
+		G_Printf( "capturelimit %i is out of range, defaulting to 0\n", g_capturelimit.integer );
+		trap_Cvar_Set( "capturelimit", "0" );
+		trap_Cvar_Update( &g_capturelimit );
 	}
 
 	if ( g_gametype.integer >= GT_CTF && g_capturelimit.integer ) {

PadWorld-Entertainment/worldofpadman-fork

code/game/g_main.c
@@ -1439,6 +1439,12 @@ static void CheckExitRules(void) {
 	}
 	*/
 
+	if (g_fraglimit.integer < 0) {
+		G_Printf("fraglimit %i is out of range, defaulting to 0\n", g_fraglimit.integer);
+		trap_Cvar_Set("fraglimit", "0");
+		trap_Cvar_Update(&g_fraglimit);
+	}
+
 	if ((g_gametype.integer == GT_LPS) && !(g_LPS_flags.integer & LPSF_PPOINTLIMIT)) {
 		int playersWithLivesLeft = 0;
 		gclient_t *tmpcl = NULL;
@@ -1501,6 +1507,12 @@ static void CheckExitRules(void) {
 				return;
 			}
 		}
+	}
+
+	if (g_capturelimit.integer < 0) {
+		G_Printf("capturelimit %i is out of range, defaulting to 0\n", g_capturelimit.integer);
+		trap_Cvar_Set("capturelimit", "0");
+		trap_Cvar_Update(&g_capturelimit);
 	}
 
 	if (g_gametype.integer >= GT_CTF && g_capturelimit.integer) {